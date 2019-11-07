3ed7a22839831234125b822737dcbf0cf003da04
Hasmukh Rathod <guru__@hotmail.com>
2019-11-07 15:53:26 +0100

    changed

--- ass.Hangman/Program.cs
+++ ass.Hangman/Program.cs
@@ -5,133 +5,164 @@ using System.Text;
 
 
 namespace ass.Hangman
-{   
-       
+{
+
     class Program
     {
 
-        
+        static Random rand;
+        static int Lives;
+        static string GameWords;
+        static int GamesWordsLength;
+        static string Namn;
+        //static undesrScore;
         //private static object arrayofWord;
 
-        static void Main(string[] args)
 
+        static List<string> ListWord = new List<string>()
         {
 
-            //bool keepAlive = true;
-            //while (keepAlive)
-            
-             
-            //string Namn;
-            ////string Random;
 
+            "lastbil",
+            "blomma",
 
-            //Console.WriteLine("Välkomen till Häng Gubbe");
+        };
+        static void Main(string[] args)
+        {
 
-            //Console.Write(" Skriv dit Namn:  ");
-            //Namn = Convert.ToString(Console.ReadLine());
-
-            //Console.Clear();
-            //Console.WriteLine(Namn + ", Välkomen till");g
-
-            ////Console.WriteLine("   Häng Gubbe   ");
-            //Console.WriteLine(" _______________");
-            //Console.WriteLine(" |      |       ");
-            //Console.WriteLine(" |     ( )      ");
-            //Console.WriteLine(" |     /|\\      "); 
-            //Console.WriteLine(" |      |       ");
-            //Console.WriteLine(" |      |       ");
-            //Console.WriteLine(" |     /|\\     ");
-            //Console.WriteLine(" |_____________ ");
-            //Console.WriteLine("   Häng Gubbe   ");
+            rand = new Random();
+            PlayHangman();
 
         }
-         
-        static void ListWords()
-   
-        { 
-            List<string> word = new List<string>();
+        static void PlayHangman()
 
-            word.Add("lastbil");
-            word.Add(" båt");
-            word.Add("stad");
-            //words.Add("lexicon");
-            //words.Add("namnmaskin");
-            //words.Add(" hus");
-            //words.Add("data");
-            word.Add("mobil");
-            word.Add("röd");
-            word.Add("z");
-
-            //foreach (string awords in word)
-            //{
-            //    Console.WriteLine(word);
-            //}
+        {
+            Lives = 10;
+            GameWords = ListWord[rand.Next(ListWord.Count() - 1)];
+            GamesWordsLength = GameWords.Length;
 
 
-            string[] FourLetterWords = new string[word.Count];
+            underScore();
 
-            for (int i = 0; i <  word.Count; i++)
+        }
+        static void underScore()
+        {
+            Console.WriteLine(" Enter");
+            Console.Write(" Skriv dit Namn:  ");
+            Namn = Convert.ToString(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine(Namn + ", Välkomen till Häng Gubbe, börja spela");
+            Console.WriteLine("");
+            Console.WriteLine("Antal Lives " + Lives + "\n");
+            Console.WriteLine("Ange dit guess ord");
+
+            string entered = Console.ReadLine();
+            string underScore = string.Empty;
+
+            for (int length = 0; length < entered.Length; length++)
             {
-                FourLetterWords[i] = word[i];
-            }
-            
-        
-            
-           
 
-            Random ran = new Random();
-            string fourword = FourLetterWords[ran.Next(1, word.Count + 1)];
-           
-            //string fourword = FourLetterWords[ran.Next(0,6);
-            //var number = ran.Next(0, 6);
-            //string randomword = arrayofWord[number];
-
-            //char[] guess = new char[randomword.Length];
-            //Console.WriteLine(" you have");
-
-
-            char[] letter = fourword.ToCharArray();
-
-            string[] emty = new string[4];
-
-            for (int a = 0; a <emty.Length; a++)
-            {
-                emty[a] = " _ ";
-            }
-
-            static void Play()
-            {
-                Console.Clear();
-                Console.WriteLine(" Välje dit ord och spela");
-                Console.WriteLine("-----------------------");
-                Console.WriteLine("");
-                Console.WriteLine("          " + emty[0] + " " + emty[1] + " " + emty[2] + " " + emty[3] + " " + emty[4]);
-                Console.WriteLine("");
-
-
-                string letterChoice = Console.ReadLine().ToUpper();
-                char[] leter = letterChoice.ToCharArray();
+                underScore = underScore + "_";
 
             }
+                 Console.WriteLine(underScore);
+                 Console.ReadKey(true);
+                 Console.WriteLine("");
+                
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                 Console.ReadLine();
+                 char guess = char.ToUpper(Console.ReadKey().KeyChar);
+            for (int i = 0; i < (GameWords.Length); i ++) 
+            {
 
-         } // END OF MAIN
-
-       //static void game()
-
-       
-
-
-
-
-
-        
-        
-
-
+                GameWords.Replace(GameWords, (" _"));
+                 Console.ReadLine();
+               //CheckedGuess(GameWords,  ("_"));
 
 
 
+
+
+                //int underScoreLength = 0;
+                //string winning = " ";
+
+
+                ////check(hemligt);
+                ////hemligt = Convert.Char(Console.ReadLine());
+                //for (int i = 0; i < (GameWords.Length); i++) ;
+                //{
+
+
+                //    if (GuessedChars.Any();
+                //    {
+
+
+
+                //    { 
+                //    else
+                //        Console.Write(" _ ");
+
+                //}
+                //else
+                //{
+                //    GameWords.Replace(GameWords, (" _"));
+                //    Console.ReadLine();
+
+            }  // End underScore
+
+
+
+
+
+            //Console.WriteLine(underScore);
+
+
+
+
+
+
+
+
+
+        }// END OF MAIN
+
+
+
+                //foreach (string awords in word)
+                ////{
+                //    Console.WriteLine(word);
+                //}
+
+                //string[] FourLetterWords = new string[word.Count];
+
+                //for (int i = 0; i < word.Count; i++)
+                //{
+                //    FourLetterWords[i] = word[i];
 
     } // END  OF CLASS
 
-}// NAME SPACE
+             //Random rand = new Random();
+             //string fourword = FourLetterWords[rand.Next(1, word.Count + 1)];
+
+             //string fourword = FourLetterWords[ran.Next(0,6);
+             //var number = ran.Next(0, 6);
+            //string randomword = arrayofWord[number];
+
+            ////char[] guess = new char[ran.Length];
+            //Console.WriteLine(" you have");
+
+
+            ////char[] letter = fourword.ToCharArray();
+
+            //string[] emty = new string[4];
+
+                //for (int a = 0; a <emty.Length; a++)
+                //{
+                //    emty[a] = " _ ";
+                //}
+
+                   //    string letterChoice = Console.ReadLine().ToUpper();
+                  //    char[] leter = letterChoice.ToCharArray();
+
+
+} // NAME SPACE          
